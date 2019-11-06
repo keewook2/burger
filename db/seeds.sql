f05cf9f2a1af53512378c0deb0ab3e7efefcce23
Kee Wook Lee <keewook2@gmail.com>
2019-11-06 10:16:49 -0500

    initial config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("chicken burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Bacon burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Spicy burger", false);
